4869d9a8133c26953e4d07f8b6b81739a0e28034
Dionathandm <plugplaysong@gmail.com>
2026-01-18 21:46:01 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,22 @@
-const CACHE_NAME = "hydramusic-v1";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+const CACHE_NAME = 'hydramusic-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  'https://dionathandm.github.io/hydramusic/musicas/A Firma Segue Lucrando_spotdown.org.mp3',
+  'https://i.ibb.co/60KP16PJ/lv-0-20260117003207.png'
+  // adicione todas as músicas e imagens aqui
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
