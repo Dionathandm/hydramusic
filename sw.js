947d9dfff10352c1f6b788655a43422a7d501736
Dionathandm <plugplaysong@gmail.com>
2026-01-18 23:25:40 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,11 @@
-const CACHE_NAME = 'hydramusic-cache-v2';
-const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  'https://dionathandm.github.io/hydramusic/musicas/A Firma Segue Lucrando_spotdown.org.mp3',
-  'https://i.ibb.co/60KP16PJ/lv-0-20260117003207.png'
-  // adicione todas as músicas e imagens aqui
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // força nova versão
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', (event) => {
+  clients.claim(); // assume controle imediatamente
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
