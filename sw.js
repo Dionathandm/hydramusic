9dd8335cc5c534591a037c7aa8eeb92ab3303c6e
Dionathandm <plugplaysong@gmail.com>
2026-01-22 17:36:41 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,39 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting(); // força nova versão
+const CACHE_NAME = "hydramusic-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  clients.claim(); // assume controle imediatamente
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
